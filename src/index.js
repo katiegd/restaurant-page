6f61370b450ef6a45a47a32e365edb41fd82e64d
Katie Duryea <155897343+katiegd@users.noreply.github.com>
2024-04-02 18:21:01 -0400

    Working on getting webpack up and running

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import "./style.css";
 
 const homeBtn = document.querySelector("#home");
+const menuBtn = document.querySelector("#menu");
+const aboutBtn = document.querySelector("#about");
 
 homeBtn.addEventListener("click", () => {
   alert("Hello! You did it!");
+});
+
+menuBtn.addEventListener("click", () => {
+  alert("Beep! Here's the menu");
+});
+
+aboutBtn.addEventListener("click", () => {
+  alert("boop! about us!");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,39 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Yum Yum Town",
-      template: "src/index.html",
-      inject: "body",
-    }),
-  ],
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Juniper",
+      template: "src/template.html",
+      inject: "body",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        loader: "file-loader",
+        type: "asset/resource",
       },
     ],
   },
